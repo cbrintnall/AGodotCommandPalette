3269db4cc363289963d338c4bb8aac34fe82de01
Leleat <atule@pm.me>
2020-03-03 22:14:25 +0100

    added a quickjump-to-line feature and the list additionally shows file loaction

--- addons/SearchSceneScriptDock/README.md
+++ addons/SearchSceneScriptDock/README.md
@@ -1,7 +1,15 @@
 # SearchScriptScenePlugin
-This plugin for the Godot game engine (3.2.X) enables you to search (case-insensitive) the open scenes and scripts to quickly switch to them.
+This plugin for the Godot game engine (3.2.X) adds a global keyboard shortcut to access a list of all open scripts and scenes which can be filtered by a search_string.
 
-Starting the search-string with \"c \" (for code) searches the open scripts.
-Using  \"s \" (for scene) only goes through the open scenes.
+After activation of the plugin reopen the project (only needed to do upon enabling the plugin the first time).
 
-Reopen the project after activating the plugin the first time it is activated.
+
+Features:
+
+Pressing Ctrl+E opens a popup, which lists all open scenes and scripts.
+The file names are preceded by a line number and followed by their respective file location.
+Ending the search_string with \" X\", where X is an interger, jumps to that line in the list.
+Starting the search-string with \"c \" (as in code) filters the open scripts.
+Starting the search-string with \"s \" (as in scene) filters the open scenes.
+
+(The filter only applies to the actual file name, so you won't get flooded with search results)
--- addons/SearchSceneScriptDock/SearchSceneScriptDock.gd
+++ addons/SearchSceneScriptDock/SearchSceneScriptDock.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	connect("popup_hide", self, "_on_popup_hide")
 
 
-# global keyboard shortcuts; 
+# global keyboard shortcuts
 # hitting ESC hides the popup
 func _unhandled_key_input(event: InputEventKey) -> void: 
 	if event.as_text() == "Control+E" and event.is_pressed():
@@ -51,34 +51,45 @@ func _on_filter_text_entered(new_txt : String) -> void:
 func _open_selection() -> void:
 	var selection = list.get_selected_items()
 	if selection:
-		var selected_file_name = list.get_item_text(selection[0])
+		var selected = list.get_item_text(selection[0])
+		var selection_without_line_number = selected.split("  ::  ")[1] 
+		var selected_file_name = selection_without_line_number.split("    >>    ")[0]
+			
 		if selected_file_name.findn(".gd") != -1 or selected_file_name.findn(".vs") != -1: # selected file is a gd/gdns/vs script
 			INTERFACE.edit_resource(open_scripts[selected_file_name])
 			INTERFACE.set_main_screen_editor("Script")
+			
 		else: # selected file is a scene file
-			INTERFACE.open_scene_from_path(open_scenes[selected_file_name])  
+			INTERFACE.open_scene_from_path(open_scenes[selected_file_name])
 			INTERFACE.set_main_screen_editor("3D") if INTERFACE.get_edited_scene_root() is Spatial else INTERFACE.set_main_screen_editor("2D")
 	hide()
 
 
+# search happens only on the actual file name
 func _update_list() -> void:
 	list.clear()
 	open_scenes.clear()
 	open_scripts.clear()
+	var search_string = filter.text as String
+	
+	var quickselect_line = 0 # typing " X" (where X is an integer) at the end of the search_string jumps to that item in the list
+	var quickselect_index = search_string.find_last(" ")
+	if quickselect_index != -1 and not search_string.ends_with(" "):
+		quickselect_line = search_string.substr(quickselect_index + 1)
+		if quickselect_line.is_valid_integer():
+			search_string.erase(quickselect_index + 1, String(quickselect_line).length())
 		
-	var search_string = filter.text
-	if search_string.begins_with("c "): # starting the search with "c " (c for code) only shows/searches scripts
-		search_string.erase(0, 2)
-		_add_scripts(search_string)
-	elif search_string.begins_with("s "): # starting with "s " (s for scene) only shows/searches scenes
-		search_string.erase(0, 2)
-		_add_scenes(search_string)
-	else: # show scripts and scenes together
-		_add_scripts(search_string)
-		_add_scenes(search_string)
+	if search_string.begins_with("c "):
+		_add_scripts(search_string.substr(2).strip_edges())
+	elif search_string.begins_with("s "):
+		_add_scenes(search_string.substr(2).strip_edges())
+	else:
+		_add_scripts(search_string.strip_edges())
+		_add_scenes(search_string.strip_edges())
 		
 	list.sort_items_by_text()
-	list.call_deferred("select", 0) # call_deferred because on a weird selection issue (highlight only goes partly) on first call of the popup whenever a project is opened
+	_count_list()
+	list.call_deferred("select", clamp(quickselect_line as int, 0, list.get_item_count() - 1)) # call_deferred because of a weird selection issue (highlight only goes partly) on first call of the popup whenever a project is opened
 
 
 func _add_scripts(search_string : String) -> void:
@@ -88,10 +99,10 @@ func _add_scripts(search_string : String) -> void:
 		if search_string:
 			if script_name.findn(search_string) != -1 :
 				open_scripts[script_name] = script
-				list.add_item(script_name, script_icon)
+				list.add_item(script_name + "    >>    " + script.resource_path.get_base_dir(), script_icon)
 		else:
 			open_scripts[script_name] = script
-			list.add_item(script_name, script_icon)
+			list.add_item(script_name + "    >>    " + script.resource_path.get_base_dir(), script_icon)
 
 
 func _add_scenes(search_string : String) -> void:
@@ -101,7 +112,12 @@ func _add_scenes(search_string : String) -> void:
 		if search_string:
 			if scene_name.findn(search_string) != -1:
 				open_scenes[scene_name] = scene_path
-				list.add_item(scene_name, scene_icon)
+				list.add_item(scene_name + "    >>    " + scene_path.get_base_dir(), scene_icon)
 		else:
 			open_scenes[scene_name] = scene_path
-			list.add_item(scene_name, scene_icon)
+			list.add_item(scene_name + "    >>    " + scene_path.get_base_dir(), scene_icon)
+
+
+func _count_list() -> void:
+	for item in list.get_item_count():
+		list.set_item_text(item, String(item) + "  ::  " + list.get_item_text(item))
