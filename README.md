1c80f5fa64462940c9e4161b1de4f3480a769998
Leleat <atule@pm.me>
2020-03-14 00:46:27 +0100

    refactor, new feat: jump to line X, code snippet and dedicated help page

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
-# SearchScriptScenePlugin
+# Command Palette
 
-This plugin for the Godot game engine 3.2.X adds a global keyboard shortcut to access a list of all (open) scripts and scenes which can be filtered by a search_string.
+This plugin for the Godot game engine 3.2.X adds a global keyboard shortcut to access a list of all (open) scripts and scenes which can be filtered by a search_string as well as code snippets.
 
 
 **Features**:
 
 - Pressing Ctrl+E opens a popup, which lists all open scenes and scripts.
 - Pressing Ctrl+E while the popup is already open and no filter is applied will switch to the previously opened file (only works for files opened with this plugin).
-- The file names are preceded by a line number.
+- The file names are preceded by a line number and followed by their file path. Activating the file path will select the file in the filesystem dock.
 - Ending the search_string with \" X\", where X is an integer, jumps to the X-th line in the list.
 - Starting the search_string with \"a \" (a for all) will show ALL scripts and scenes.
 - Starting the search-string with \"c \" (c for code) will show all OPEN scripts.
 - Starting the search-string with \"s \" (s for scene) will show all OPEN scenes.
 - Starting the search_string with \"ac \" or \"ca \" will show ALL SCRIPTS.
 - Starting the search_string with \"as \" or \"sa \" will show ALL SCENES.
+- Starting the search_string with \": \" followed by an integer will jump to that line in the script editor.
+- Starting the search_string with \"_ \" will show the available code snippets. Activating a snippet will insert it at the cursor position. Custom snippets can be created by editing CodeSnippets.cfg. Follow the key naming scheme. Take a look at _paste_code_snippet(), if you want to know how the snippet parts are merged. By default a few virtual methods are implemented.
+
 - Opening a script also opens the scene, which the script is attached to. It only works if the script is attached to the scene root. This gives you autocompletion on the Node(Paths) and their methods.
 
-(The filter only applies to the actual file name, so you won't get flooded with search results)
+- A copy button is available to the right of the filter. This way you can quickly copy the file path or code snippet by tabbing from the filter box.
 
-**Beware! Depending on your PC, performance can tank because this plugin scans the entire project structure to list all scripts, scenes and build its file tree (multiple times).** 
+(The filter only applies to the actual file name, so you won't get flooded with search results)
 
 **Installation**:
 
 Either download it from the official Godot AssetLib (within Godot itself) or download the addons folder from GitHub (https://github.com/Leleat/SearchScriptScenePlugin) and move it to the root (res://) of your project. Enable the plugin in the project settings.
 
-![Preview](SearchSceneScriptPopup/assets/screenshot_preview.png)
+![Preview](addons/CommandPalettePopup/assets/screenshot_preview.png)
---
+++ addons/CommandPalettePopup/CommandPalettePopup.gd
@@ -0,0 +1,387 @@
+tool
+extends Popup
+
+
+onready var filter = $PanelContainer/Panel/MarginContainer/Content/VBoxContainer/SearchFilter/Filter
+onready var item_list = $PanelContainer/Panel/MarginContainer/Content/VBoxContainer/MarginContainer/HSplitContainer/ItemList
+onready var copy_path_button =$PanelContainer/Panel/MarginContainer/Content/VBoxContainer/SearchFilter/CopyButton
+onready var info_box = $PanelContainer/Panel/MarginContainer/Content/VBoxContainer/MarginContainer/HSplitContainer/RightInfoBox
+
+# after making changes in the inspector turn the plugin off and then on again in the project settings or reopen the project to apply the changes
+export (Vector2) var popup_size
+export (String) var keyboard_shortcut # go to "Editor > Editor Settings... > Shortcuts > Bindings" to see how a shortcut looks as a String
+export (Color) var secondary_color = Color(1, 1, 1, .3) # color for 3rd column
+
+var current_file # file names as String
+var last_file  # switch between last 2 files opened with this plugin
+var files_are_updating = false
+var files : Dictionary # holds ALL scenes and scripts with different properties, see _update_files_dictionary()
+enum FILTER {ALL_SCENES_AND_SCRIPTS, ALL_SCENES, ALL_SCRIPTS, ALL_OPEN_SCENES, ALL_OPEN_SCRIPTS, HELP}
+var code_snippets : ConfigFile = ConfigFile.new()
+
+var PLUGIN : EditorPlugin
+var INTERFACE : EditorInterface
+var EDITOR : ScriptEditor
+var FILE_SYSTEM : EditorFileSystem
+
+
+func _ready() -> void:
+	connect("popup_hide", self, "_on_popup_hide")
+	filter.connect("text_changed", self, "_on_filter_text_changed")
+	filter.connect("text_entered", self, "_on_filter_text_entered")
+	item_list.connect("item_activated", self, "_on_item_list_activated")
+	item_list.connect("item_selected", self, "_on_item_list_selected")
+	copy_path_button.connect("pressed", self, "_on_copy_button_pressed")
+	FILE_SYSTEM.connect("filesystem_changed", self, "_on_filesystem_changed")
+	PLUGIN.connect("main_screen_changed", self, "_on_main_screen_changed")
+		
+	var error = code_snippets.load("res://addons/CommandPalettePopup/CodeSnippets.cfg")
+	if error != OK:
+		print("Error loading the code_snippets. Error code: %s" % error)
+
+
+func _unhandled_key_input(event: InputEventKey) -> void:
+	if event.as_text() == keyboard_shortcut and event.pressed and visible and not filter.text and filter.has_focus():
+		# switch between the last two opened files (only when opened via this plugin)
+		if last_file:
+			_open_scene(files[last_file].File_Path) if files[last_file].Type == "Scene" else _open_script(files[last_file].ScriptResource)
+		hide()
+		
+	elif event.as_text() == keyboard_shortcut and event.pressed:
+		rect_size = popup_size
+		popup_centered()
+		filter.grab_focus()
+		_update_popup_list()
+
+
+func _on_main_screen_changed(new_screen : String) -> void:
+# set current_file; only once on startup of the project:
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	for path in INTERFACE.get_open_scenes():
+	# INTERFACE.get_edited_scene_root().name removes all leading underscores, so we also remove them
+		var file_name = path.get_file()
+		while file_name.begins_with("_"):
+			file_name.erase(0, 1)
+		if file_name.begins_with(INTERFACE.get_edited_scene_root().name + "."):
+			current_file = path.get_file()
+			break
+	if PLUGIN.is_connected("main_screen_changed", self, "_on_main_screen_changed"):
+		PLUGIN.disconnect("main_screen_changed", self, "_on_main_screen_changed")
+
+
+func _on_filesystem_changed() -> void:
+# to prevent unnecessarily updating the dictionary cause the signal gets fired multiple times
+	if not files_are_updating:
+		files_are_updating = true
+		_update_files_dictionary(FILE_SYSTEM.get_filesystem(), true)
+		yield(get_tree().create_timer(0.1), "timeout")
+		files_are_updating = false
+
+
+func _on_copy_button_pressed() -> void:
+	var selection = item_list.get_selected_items()
+	if selection:
+		if filter.text.begins_with("_ "): # code snippets are shown
+			var use_type_hints = INTERFACE.get_editor_settings().get_setting("text_editor/completion/add_type_hints")
+			var snippet_name = item_list.get_item_text(selection[0]).strip_edges()
+			OS.clipboard = code_snippets.get_value(snippet_name, "signature")
+			OS.clipboard += code_snippets.get_value(snippet_name, "type_hint") if use_type_hints else code_snippets.get_value(snippet_name, "no_type_hint")
+		else:
+			if selection[0] % item_list.max_columns == 1: # selection is a file name
+				var path = files[item_list.get_item_text(selection[0]).strip_edges()].File_Path
+				OS.clipboard = "\"" + path + "\""
+	hide()
+
+
+func _on_popup_hide() -> void:
+	filter.clear()
+
+
+func _on_item_list_selected(index : int) -> void:
+	if index % item_list.max_columns == 1 and filter.text.begins_with("_ "): # selection is a code_snippet
+		_build_info_box(item_list.get_item_text(index).strip_edges())
+
+
+func _on_item_list_activated(index : int) -> void:
+	var selected_name = item_list.get_item_text(index).strip_edges()
+	if filter.text.begins_with("_ "): # code snippets
+		_paste_code_snippet(selected_name)
+	elif index % item_list.max_columns == 1: # file names
+		_open_selection(selected_name)
+	elif index % item_list.max_columns == 2: # file paths 
+		var file_name = item_list.get_item_text(index - 1).strip_edges()
+		INTERFACE.select_file(files[file_name].File_Path)
+	hide()
+
+
+func _on_filter_text_changed(new_txt : String) -> void:
+	_update_popup_list()
+
+
+func _on_filter_text_entered(new_txt : String) -> void:
+	var selection = item_list.get_selected_items()
+	if selection:
+		var selected_name = item_list.get_item_text(selection[0]).strip_edges()
+		if filter.text.begins_with("_ "): # code snippets
+				_paste_code_snippet(selected_name)
+		else:
+			_open_selection(selected_name) # file
+	hide()
+
+
+func _open_selection(selected_name : String) -> void:
+	if files[selected_name].Type == "Script": 
+		_open_script(files[selected_name].ScriptResource)
+	else: 
+		_open_scene(files[selected_name].File_Path)
+
+
+func _open_script(script : Script) -> void:
+	if script.has_meta("Scene_Path"):
+		INTERFACE.open_scene_from_path(script.get_meta("Scene_Path"))
+		
+	INTERFACE.edit_resource(script)
+	INTERFACE.call_deferred("set_main_screen_editor", "Script")
+		
+	last_file = current_file
+	current_file = script.resource_path.get_file()
+
+
+func _open_scene(path : String) -> void:
+	INTERFACE.open_scene_from_path(path)
+	INTERFACE.call_deferred("set_main_screen_editor", "3D") if INTERFACE.get_edited_scene_root() is Spatial else INTERFACE.call_deferred("set_main_screen_editor", "2D")
+		
+	last_file = current_file
+	current_file = path.get_file()
+
+
+func _update_files_dictionary(folder : EditorFileSystemDirectory, reset : bool = false) -> void:
+	if reset:
+		files.clear()
+		
+	var script_icon = INTERFACE.get_base_control().get_icon("Script", "EditorIcons")
+	for file in folder.get_file_count():
+		var file_path = folder.get_file_path(file)
+		var file_type = FILE_SYSTEM.get_file_type(file_path)
+		var file_name = folder.get_file(file)
+			
+		if file_type.findn("Script") != -1:
+			files[file_name] = { "Type" : "Script", "File_Path" : file_path, "ScriptResource" : load(file_path), "Icon" : script_icon}
+			
+		elif file_type.findn("Scene") != -1: 
+			files[file_name] = {"Type" : "Scene", "File_Path" : file_path, "Icon" : null}
+				
+			var scene = load(file_path).instance()
+			files[file_name].Icon = INTERFACE.get_base_control().get_icon(scene.get_class(), "EditorIcons")
+			var attached_script = scene.get_script()
+			if attached_script:
+				attached_script.set_meta("Scene_Path", file_path)
+			scene.free()
+		
+	for subdir in folder.get_subdir_count():
+		_update_files_dictionary(folder.get_subdir(subdir))
+
+
+func _update_popup_list() -> void:
+	item_list.clear()
+	item_list.visible = true
+	info_box.bbcode_text = "No info..."
+	info_box.visible = false
+	var search_string = filter.text
+		
+	# typing " X" at the end of the search_string jumps to the X-th item in the list
+	var quickselect_line = 0
+	var qs_starts_at = search_string.find_last(" ")
+	if qs_starts_at != -1 and not search_string.ends_with(" ") and not search_string.begins_with(": "):
+		quickselect_line = search_string.substr(qs_starts_at + 1)
+		if quickselect_line.is_valid_integer():
+			search_string.erase(qs_starts_at + 1, String(quickselect_line).length())
+		
+	if search_string == "?":
+		_build_help()
+		info_box.visible = true
+		item_list.visible = false
+		return
+		
+	elif search_string.begins_with(": "): # go to line 
+		var number = search_string.substr(2).strip_edges()
+		if number.is_valid_integer():
+			var max_lines = EDITOR.get_current_script().source_code.count("\n")
+			EDITOR.goto_line(clamp(number as int - 1, 0, max_lines))
+		return
+		
+	elif search_string.begins_with("_ "): # show code snippets
+		_build_method_list(search_string.substr(2).strip_edges())
+		
+	elif search_string.begins_with("a "): # show all scripts and scenes
+		_build_item_list(search_string.substr(2).strip_edges(), FILTER.ALL_SCENES_AND_SCRIPTS)
+		
+	elif search_string.begins_with("ac ") or search_string.begins_with("ca "): # show all scripts
+		_build_item_list(search_string.substr(3).strip_edges(), FILTER.ALL_SCRIPTS)
+		
+	elif search_string.begins_with("as ") or search_string.begins_with("sa "): # show all scenes
+		_build_item_list(search_string.substr(3).strip_edges(), FILTER.ALL_SCENES)
+		
+	elif search_string.begins_with("c "): # show open scripts
+		_build_item_list(search_string.substr(2).strip_edges(), FILTER.ALL_OPEN_SCRIPTS)
+		
+	elif search_string.begins_with("s "): # show open scenes
+		_build_item_list(search_string.substr(2).strip_edges(), FILTER.ALL_OPEN_SCENES)
+		
+	else: # show all open scenes and scripts
+		_build_item_list(search_string.strip_edges())
+		
+	quickselect_line = clamp(quickselect_line as int, 0, item_list.get_item_count() / item_list.max_columns - 1)
+	if item_list.get_item_count() > 0:
+		item_list.select(quickselect_line * item_list.max_columns + 1)
+		item_list.ensure_current_is_visible()
+		if filter.text.begins_with("_ "): # code snippets
+			_build_info_box(item_list.get_item_text(item_list.get_selected_items()[0]).strip_edges())
+
+
+func _build_item_list(search_string : String, special_filter : int = -1) -> void:
+	var list : Array
+	match special_filter:
+		FILTER.ALL_SCENES_AND_SCRIPTS:
+			for file in files:
+				if search_string:
+					if file.findn(search_string) != -1:
+						list.push_back(file)
+				else:
+					list.push_back(file)
+			
+		FILTER.ALL_SCRIPTS:
+			for file in files:
+				if files[file].Type == "Script":
+					if search_string:
+						if file.findn(search_string) != -1:
+							list.push_back(file)
+					else:
+						list.push_back(file)
+			
+		FILTER.ALL_SCENES:
+			for file in files:
+				if files[file].Type == "Scene":
+					if search_string:
+						if file.findn(search_string) != -1:
+							list.push_back(file)
+					else:
+						list.push_back(file)
+			
+		FILTER.ALL_OPEN_SCENES:
+			var open_scenes = INTERFACE.get_open_scenes()
+			for path in open_scenes:
+				var scene_name = path.get_file()
+				if search_string:
+					if scene_name.findn(search_string) != -1:
+						list.push_back(scene_name)
+				else:
+					list.push_back(scene_name)
+			
+		FILTER.ALL_OPEN_SCRIPTS:
+			var open_scripts = EDITOR.get_open_scripts()
+			for script in open_scripts:
+				var script_name = script.resource_path.get_file()
+				if search_string:
+					if script_name.findn(search_string) != -1:
+						list.push_back(script_name)
+						
+				else:
+					list.push_back(script_name)
+			
+		_:
+			var open_scenes = INTERFACE.get_open_scenes()
+			for path in open_scenes:
+				var scene_name = path.get_file()
+				if search_string:
+					if scene_name.findn(search_string) != -1:
+						list.push_back(scene_name)
+				else:
+					list.push_back(scene_name)
+				
+			var open_scripts = EDITOR.get_open_scripts()
+			for script in open_scripts:
+				var script_name = script.resource_path.get_file()
+				if search_string:
+					if script_name.findn(search_string) != -1:
+						list.push_back(script_name)
+				else:
+					list.push_back(script_name)
+		
+	list.sort()
+	for index in list.size():
+		item_list.add_item(" " + String(index) + "  :: ", null, false)
+		item_list.add_item(" " + list[index], files[list[index]].Icon)
+		var file_path = files[list[index]].File_Path.get_base_dir()
+		item_list.add_item(" - " + file_path.substr(0, 6) + " - " + file_path.substr(6).replace("/", " - "))
+		item_list.set_item_custom_fg_color(item_list.get_item_count() - 1, secondary_color)
+
+
+func _build_help() -> void:
+	var file = File.new()
+	file.open("res://addons/CommandPalettePopup/Help.txt", File.READ)
+	info_box.bbcode_text = file.get_as_text()
+	file.close()
+
+
+func _build_method_list(filtered_name : String) -> void:
+	var counter = 0
+	for method_name in code_snippets.get_sections():
+		if filtered_name:
+			if method_name.findn(filtered_name) != -1:
+				item_list.add_item(" " + String(counter) + "  :: ", null, false)
+				item_list.add_item(" " + method_name)
+				item_list.add_item(code_snippets.get_value(method_name, "additional_info"), null, false) if code_snippets.has_section_key(method_name, "additional_info") else item_list.add_item("")
+				item_list.set_item_custom_fg_color(item_list.get_item_count() - 1, secondary_color)
+				counter += 1
+		else:
+			item_list.add_item(" " + String(counter) + "  :: ", null, false)
+			item_list.add_item(" " + method_name)
+			item_list.add_item(code_snippets.get_value(method_name, "additional_info"), null, false) if code_snippets.has_section_key(method_name, "additional_info") else item_list.add_item("")
+			item_list.set_item_custom_fg_color(item_list.get_item_count() - 1, secondary_color)
+			counter += 1
+
+
+func _build_info_box(snippet_name : String) -> void:
+	info_box.bbcode_text = "No info..."
+		
+	if code_snippets.has_section_key(snippet_name, "description"):
+		info_box.bbcode_text = code_snippets.get_value(snippet_name, "description")
+		info_box.visible = true
+
+
+func _paste_code_snippet(snippet_name : String) -> void:
+	var TextEdit_index = 0
+	for script in EDITOR.get_open_scripts():
+		if script == EDITOR.get_current_script():
+			break
+		TextEdit_index += 1
+	var code_editor = EDITOR.get_child(0).get_child(1).get_child(1).get_child(TextEdit_index).get_child(0).get_child(0).get_child(0) # TextEdit of current script
+	var use_type_hints = INTERFACE.get_editor_settings().get_setting("text_editor/completion/add_type_hints")
+	var code_snippet = ""
+	code_snippet +=  code_snippets.get_value(snippet_name, "signature")
+	code_snippet += code_snippets.get_value(snippet_name, "type_hint") if use_type_hints else code_snippets.get_value(snippet_name, "no_type_hint")
+	code_editor.insert_text_at_cursor(code_snippet)
+# Alternative way of snippet implementation: copy to clipboard and creating Ctrl+V InputEvent
+#	var old_clipboard = OS.clipboard 
+#	OS.clipboard = ""
+#
+#	var use_type_hints = INTERFACE.get_editor_settings().get_setting("text_editor/completion/add_type_hints")
+#	OS.clipboard +=  code_snippets.get_value(snippet_name, "signature")
+#	OS.clipboard += code_snippets.get_value(snippet_name, "type_hint") if use_type_hints else code_snippets.get_value(snippet_name, "no_type_hint")
+#
+#	call_deferred("_paste_helper_method", old_clipboard)
+#
+#
+#func _paste_helper_method(old_clipboard : String) -> void:
+#	var paste_key_combo = InputEventKey.new()
+#	if OS.get_name() == "OSX":
+#		paste_key_combo.command = true
+#	else:
+#		paste_key_combo.control = true
+#	paste_key_combo.scancode = KEY_V
+#	paste_key_combo.pressed = true
+#	Input.parse_input_event(paste_key_combo)
+#
+#	OS.clipboard = old_clipboard
